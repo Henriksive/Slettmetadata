baf3de508d760236965a12a09fed80e0c8aa9626
Henrik Sivertsgård <henriksive@gmail.com>
2018-03-21 10:53:57 +0100

    initial commit

---
+++ SlettMetadata/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace SlettMetadata
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				var filename = args[0];
+				using (var package = WordprocessingDocument.Open(filename, true))
+				{
+					// modify properties
+					package.PackageProperties.Creator = null;
+					package.PackageProperties.LastModifiedBy = null;
+					package.PackageProperties.Revision = null;
+				}
+				Console.WriteLine("Slettet metadata for filen: " + filename);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("En feil oppstod: " + e.Message);
+			}
+		}
+	}
+}
